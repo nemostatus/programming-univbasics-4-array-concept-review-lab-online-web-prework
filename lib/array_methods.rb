5f4fcf176af4a67ccce286319528c68c7d46eb4b
Matthew Corralejo <mattcorralejo1@gmail.com>
2019-11-29 05:11:13 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   return array[value_to_find]
 end
-find_element_index([1,2,3,4,5])
+find_element_index([1,2,3,4,5],0)
 
 def find_max_value(array)
   return array.max
